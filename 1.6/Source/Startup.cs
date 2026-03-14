68d043a5b0a304d79033399ceccc181ee88e89c6
Taranchuk <nfhfyxer@gmail.com>
2026-03-14 21:54:57 +0200

    job fixes

--- 1.6/Source/Startup.cs
+++ 1.6/Source/Startup.cs
@@ -10,29 +10,72 @@ namespace PerspectiveShift
     {
         static Startup()
         {
-            foreach (var thinkTreeDef in DefDatabase<ThinkTreeDef>.AllDefsListForReading)
-            {
-                var rootNode = thinkTreeDef.thinkRoot;
-                if (rootNode?.subNodes == null) continue;
+            PatchThinkTreeDefs();
+        }
 
-                var newNode = new ThinkNode_ConditionalAvatarDrafted
+        public static void PatchThinkTreeDefs()
+        {
+            var thinkTreeDefs = DefDatabase<ThinkTreeDef>.AllDefsListForReading;
+            foreach (var thinkTreeDef in thinkTreeDefs)
+            {
+                if (thinkTreeDef.defName == "Downed") continue;
+                var rootNode = thinkTreeDef.thinkRoot;
+                if (rootNode == null || rootNode.subNodes == null) continue;
+
+                int isColonist = rootNode.subNodes.FindIndex(node => node.GetType() == typeof(ThinkNode_ConditionalColonist));
+                if (isColonist >= 0)
                 {
-                    subNodes = new List<ThinkNode>
+                    InsertNodeAt(rootNode.subNodes, isColonist);
+                }
+                else
+                {
+                    int queuedJobNodeIndex = rootNode.subNodes.FindIndex(node => node.GetType() == typeof(ThinkNode_QueuedJob));
+                    if (queuedJobNodeIndex >= 0)
                     {
-                        new ThinkNode_QueuedJob(),
-                        new ThinkNode_Tagger
+                        InsertNodeAt(rootNode.subNodes, queuedJobNodeIndex);
+                    }
+                    else
+                    {
+                        int subtreeNodeIndex = rootNode.subNodes.FindIndex(node => node.GetType() == typeof(ThinkNode_Subtree) &&
+                            (node as ThinkNode_Subtree)?.treeDef.defName == "LordDuty");
+
+                        if (subtreeNodeIndex >= 0)
                         {
-                            tagToGive = JobTag.DraftedOrder,
-                            subNodes = new List<ThinkNode>
+                            InsertNodeAt(rootNode.subNodes, subtreeNodeIndex);
+                        }
+                        else
+                        {
+                            var revenantIndex = rootNode.subNodes.FindIndex(node => node.GetType() == typeof(ThinkNode_ConditionalRevenantState));
+                            if (revenantIndex >= 0)
                             {
-                                new JobGiver_MoveToStandable(),
-                                new JobGiver_Orders()
+                                InsertNodeAt(rootNode.subNodes, revenantIndex);
                             }
                         }
                     }
-                };
-                rootNode.subNodes.Insert(0, newNode);
+                }
             }
+        }
+
+        private static void InsertNodeAt(List<ThinkNode> subNodes, int index)
+        {
+            var newNode = new ThinkNode_ConditionalAvatarDrafted
+            {
+                subNodes = new List<ThinkNode>
+                {
+                    new ThinkNode_QueuedJob(),
+                    new ThinkNode_Tagger
+                    {
+                        tagToGive = JobTag.DraftedOrder,
+                        subNodes = new List<ThinkNode>
+                        {
+                            new JobGiver_MoveToStandable(),
+                            new JobGiver_Orders()
+                        }
+                    }
+                }
+            };
+
+            subNodes.Insert(index, newNode);
         }
     }
 
