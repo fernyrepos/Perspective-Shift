cbbeff2c1d4f7b96de04c6ccab5283044396f845
Taranchuk <nfhfyxer@gmail.com>
2026-03-16 03:13:59 +0200

    button adjustment

--- 1.6/Source/HarmonyPatches/Dialog_Options_Patch.cs
+++ 1.6/Source/HarmonyPatches/Dialog_Options_Patch.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
 using RimWorld;
 using UnityEngine;
@@ -8,9 +11,25 @@ namespace PerspectiveShift
     [HarmonyPatch(typeof(Dialog_Options), "DoGameplayOptions")]
     public static class Dialog_Options_DoGameplayOptions_Patch
     {
-        public static void Postfix(Listing_Standard listing)
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            if (!State.permadeath)
+            bool injected = false;
+            foreach (var inst in instructions)
+            {
+                if (!injected && inst.opcode == OpCodes.Ldstr && (string)inst.operand == "MaxNumberOfPlayerSettlements")
+                {
+                    yield return new CodeInstruction(OpCodes.Ldarg_1);
+                    yield return new CodeInstruction(OpCodes.Call,
+                        AccessTools.Method(typeof(Dialog_Options_DoGameplayOptions_Patch), nameof(InjectButton)));
+                    injected = true;
+                }
+                yield return inst;
+            }
+        }
+
+        private static void InjectButton(Listing_Standard listing)
+        {
+            if (!State.permadeath && Current.ProgramState == ProgramState.Playing)
             {
                 if (listing.ButtonTextLabeledPct("PS_PerspectiveSettings".Translate(), "PS_Modify".Translate(), 0.6f, TextAnchor.UpperLeft))
                 {
