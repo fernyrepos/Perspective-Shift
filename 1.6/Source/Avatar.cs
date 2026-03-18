0ce08fb0ea02f7839c8748a321ce74d78dc5bd20
Taranchuk <nfhfyxer@gmail.com>
2026-03-18 05:11:41 +0200

    disabled meditation job

--- 1.6/Source/Avatar.cs
+++ 1.6/Source/Avatar.cs
@@ -1502,6 +1502,7 @@ namespace PerspectiveShift
                 }
             }
 
+            /*
             if (target is Building building && MeditationUtility.IsValidMeditationBuildingForPawn(building, pawn))
             {
                 Job job;
@@ -1520,7 +1521,7 @@ namespace PerspectiveShift
                 }
                 job.ignoreJoyTimeAssignment = true;
                 if (TryStartForcedJob(job)) return true;
-            }
+            }*/
 
             if (pawn.workSettings != null)
             {
